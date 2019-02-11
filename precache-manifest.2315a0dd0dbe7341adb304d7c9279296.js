97a74f5e306d9f9f238d950286204b33fc890da1
Angela Tsang <tsangangela_@hotmail.com>
2019-02-10 23:33:35 -0800

    Updates

---
+++ precache-manifest.2315a0dd0dbe7341adb304d7c9279296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "669e009fa137538535cc",
+    "url": "/connect-four/static/css/main.6e699e09.chunk.css"
+  },
+  {
+    "revision": "669e009fa137538535cc",
+    "url": "/connect-four/static/js/main.669e009f.chunk.js"
+  },
+  {
+    "revision": "8d65ddbd71c248ac891f",
+    "url": "/connect-four/static/js/runtime~main.8d65ddbd.js"
+  },
+  {
+    "revision": "cec5852ee431cc10e713",
+    "url": "/connect-four/static/js/2.cec5852e.chunk.js"
+  },
+  {
+    "revision": "ad86a93d1a193ec1aaf64e831bda6c37",
+    "url": "/connect-four/index.html"
+  }
+];
